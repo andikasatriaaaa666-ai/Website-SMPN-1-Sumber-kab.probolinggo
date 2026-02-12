0d995821c19a6de23be9f091f544b13da29b8643
Andika Satria Oetama <andikasatriaaaa666@gmail.com>
2026-02-12 14:17:23 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/* waktu */
-setInterval(() => {
-  getElementById('jam').innerHTML = `
-  ${new Date().getHours()}:
-  ${new Date().getMinutes()}:
-  ${new Date().getSeconds()}`
-}, 1000);
 
 /* navbar slide */
 const menuToggle = document.querySelector('.menu-toggle input');
